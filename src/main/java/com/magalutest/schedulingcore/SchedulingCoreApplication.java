223be7c5f4ded8b3421eebafdac5f83ec53b6df4
Henrique Sylvestre <henriquesylvestresilva@gmail.com>
2021-04-10 16:44:53 -0300

    Criação do projeto Maven utilizando Java 14 e Spring boot

---
+++ src/main/java/com/magalutest/schedulingcore/SchedulingCoreApplication.java
@@ -0,0 +1,13 @@
+package com.magalutest.schedulingcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magalutest/schedulingcore/SchedulingCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magalutest.schedulingcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
